7dbdf8d741538eb2b19af0ce0eb657dc286c264f
Frederic FLIPO <fred.web24@gmail.com>
2024-12-04 18:51:48 +0100

    nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,11 +7,10 @@ function Header() {
     <div className='navCont'>
         <div className='navCont__block'>
           <NavLink className='navCont__block--link' to="/#welcome">Accueil</NavLink>
-          <NavLink className='navCont__block--link' to="/#contact">Contact_D</NavLink>
-          <NavLink className='navCont__block--link2' to="/#services" smooth={true} duration={500}>Services</NavLink>
-          <NavLink className='navCont__block--link2' to="/#portfolio" smooth={true} duration={500}>Portfolio</NavLink>
-          <NavLink className='navCont__block--link2' to="/#competences" smooth={true} duration={500}>Compétences</NavLink>
-          <NavLink className='navCont__block--link2' to="/#contact" smooth={true} duration={500}>Contact</NavLink>
+          <NavLink className='navCont__block--link2' to="/#services">Services</NavLink>
+          <NavLink className='navCont__block--link2' to="/#portfolio">Portfolio</NavLink>
+          <NavLink className='navCont__block--link2' to="/#competences">Compétences</NavLink>
+          <NavLink className='navCont__block--link2' to="/#contact">Contact</NavLink>
           <NavLink className='navCont__block--link' to="/APropos">A-Propos</NavLink>
         </div>
     </div>
